27b164bcff57740e6898bba196672f81f25c294f
George Snelling <georgesn@gmail.com>
2013-06-18 13:24:02 -0700

    sorta works

--- tipe.js
+++ tipe.js
@@ -10,47 +10,57 @@
 
 // Main
 function tipe(v) {
-  return tipeMap[typeof v]
-    || tipeMap[Object.prototype.toString.call(v)]
+  var typeofSane, className
+
+  // Give typeof first crack
+  typeofSane = tipeMap[typeof v]
+  if (typeofSane) return typeofSane
+
+  // See what Object.toString thinks: it returns '[object Constructor]'
+  className = Object.prototype.toString.call(v)
+  className = className.slice(0, className.length - 1).slice(8)
+
+  return tipeMap[className]
     || (v ? 'object' : 'null')
 }
 
 
-// Base map of values to their tipes
+// Map of value types to their tipes
 var tipeMap = {
   'undefined': 'undefined',
   'number': 'number',
   'boolean': 'boolean',
   'string': 'string',
-  '[object Function]': 'function',
-  '[object RegExp]': 'regexp',
-  '[object Array]': 'array',
-  '[object Date]': 'date',
-  '[object Error]': 'error',
+  'function': 'function',
+  'RegExp': 'regexp',
+  'Array': 'array',
+  'Date': 'date',
+  'Error': 'error',
 }
 
 
-// Add a user-specfied class to the tipeMap
+// Add a user-specfied class to the tipeMap.
 // Class constructor must implement toString()
 tipe.add = function(className, tipeName) {
-  tipeMap['[object ' + className + ']'] = tipeName
+  if ('Object' === className || tipeMap[className]) return // ddt
+  tipeMap[className] = tipeName
   addIsMethod(tipeName)
 }
 
 
-// Add tipe.isFoo() methods: tipe.isString(v), tipe.isPoodle(v)
+// Sweeten with tipe.isString(v), tipe.isPoodle(v), etc.
 function addIsMethod(tipeName) {
   var properCaseTipeName = tipeName.charAt(0).toUpperCase() + tipeName.slice(1)
   tipe['is' + properCaseTipeName] = function(v) {
-    return tipe(v) === t  // wtf t?
+    return tipe(v) === tipeName
   }
 }
 
 
-// Add the is methods
-tipeMap.forEach(function(t) {
-  addIsMethod(tipeMap[t])
-})
+// Add sugar
+for (var key in tipeMap) {
+  addIsMethod(tipeMap[key])
+}
 
 
 module.exports = tipe
